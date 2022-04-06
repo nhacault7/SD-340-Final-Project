764fbe2cddb7aa1a8db458bfed6d9c102806bf49
Nicholas Hacault <nicholashacault@student.mitt.ca>
2022-04-06 00:53:56 -0500

    Add Managers To Notification Controller

--- Finalproject/Controllers/NotificationsController.cs
+++ Finalproject/Controllers/NotificationsController.cs
@@ -8,22 +8,27 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Finalproject.Data;
 using Finalproject.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace Finalproject.Controllers
 {
     public class NotificationsController : Controller
     {
-        private readonly ApplicationDbContext _context;
+        private ApplicationDbContext _db;
+        private UserManager<ApplicationUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
 
-        public NotificationsController(ApplicationDbContext context)
+        public NotificationsController(ApplicationDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
-            _context = context;
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
         }
 
         // GET: Notifications
         public async Task<IActionResult> Index()
         {
-            return View(await _context.Notifications.ToListAsync());
+            return View(await _db.Notifications.ToListAsync());
         }
     }
 }
