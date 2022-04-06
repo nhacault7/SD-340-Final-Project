a98b2ced8cbb9fbfc8a4aa741c7eebc649a09a05
Nicholas Hacault <nicholashacault@student.mitt.ca>
2022-04-06 00:50:40 -0500

    Add Notification Controller Initial Setup

---
+++ Finalproject/Controllers/NotificationsController.cs
@@ -0,0 +1,29 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Finalproject.Data;
+using Finalproject.Models;
+
+namespace Finalproject.Controllers
+{
+    public class NotificationsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NotificationsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Notifications
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Notifications.ToListAsync());
+        }
+    }
+}
