e68989e80b49fe57eb992aa3747fc86aebce4449
Ling Liu <lingliu1123@gmail.com>
2022-04-08 13:32:55 -0400

    Add ExceededBudgetProject function to Dashboard page

--- Finalproject/Controllers/ProjectsExceededBudgetController.cs
+++ Finalproject/Controllers/ProjectsExceededBudgetController.cs
@@ -1,31 +1,31 @@
-﻿//using Finalproject.Data;
-//using Finalproject.Models;
-//using Microsoft.AspNetCore.Identity;
-//using Microsoft.AspNetCore.Mvc;
-//using Microsoft.EntityFrameworkCore;
+﻿using Finalproject.Data;
+using Finalproject.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
-//namespace Finalproject.Controllers
-//{
-//    public class ProjectsExceededBudgetController : Controller
-//    {
-//        private ApplicationDbContext _db;
-//        private UserManager<ApplicationUser> _userManager;
-//        private RoleManager<IdentityRole> _roleManager;
+namespace Finalproject.Controllers
+{
+    public class ProjectsExceededBudgetController : Controller
+    {
+        private ApplicationDbContext _db;
+        private UserManager<ApplicationUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
 
-//        public ProjectsExceededBudgetController(ApplicationDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
-//        {
-//            _db = db;
-//            _userManager = userManager;
-//            _roleManager = roleManager;
-//        }
-//        public IActionResult Index()
-//        {
-//            var project = _db.Projects.Include(u => u.UserProjects).
-//                          ThenInclude(u => u.User).
-//                          Where(p => p.TotalCost > p.Budget).ToList();
+        public ProjectsExceededBudgetController(ApplicationDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+        public IActionResult ExceedBudgetProjects()
+        {
+            var project = _db.Projects.Include(u => u.UserProjects).
+                          ThenInclude(u => u.User).
+                          Where(p => p.TotalCost > p.Budget).ToList();
 
 
-//            return View("ProjectsExceededBudget", project);
-//        }
-//    }
-//}
+            return View("ProjectsExceededBudget", project);
+        }
+    }
+}
