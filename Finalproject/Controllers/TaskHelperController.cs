f7a1bb9545643da6e4847ca579639f04f125973b
hhb <hhbryan@126.com>
2022-04-08 20:37:11 -0500

    added create method and view for TaskHelperController

--- Finalproject/Controllers/TaskHelperController.cs
+++ Finalproject/Controllers/TaskHelperController.cs
@@ -1,10 +1,22 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Finalproject.Data;
+using Finalproject.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Finalproject.Controllers
 {
     public class TaskHelperController : Controller
     {
+        private ApplicationDbContext _db;
+        private UserManager<ApplicationUser> _userManager;
+
+        public TaskHelperController(ApplicationDbContext db, UserManager<ApplicationUser> userManager)
+        {
+            _db = db;
+            _userManager = userManager;
+        }
         // GET: TaskHelperController
         public ActionResult Index()
         {
@@ -18,8 +30,17 @@ namespace Finalproject.Controllers
         }
 
         // GET: TaskHelperController/Create
-        public ActionResult Create()
+        public ActionResult Create(int projectId)
         {
+            ViewBag.projectId = projectId;
+            List<SelectListItem> priorities = new List<SelectListItem>
+            {
+                new SelectListItem(){ Text = "Urgent", Value = Priority.Urgent.ToString() },
+                new SelectListItem(){ Text = "High", Value = Priority.High.ToString() },
+                new SelectListItem(){ Text = "Medium", Value = Priority.Medium.ToString() },
+                new SelectListItem(){ Text = "Low", Value = Priority.Low.ToString() }
+            };
+            ViewBag.priorityList = priorities;
             return View();
         }
 
@@ -30,7 +51,30 @@ namespace Finalproject.Controllers
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                if ( ModelState.IsValid )
+                {
+                    ProjectTask newTask = new ProjectTask();
+                    newTask.Name = collection["Name"].ToString();
+                    newTask.Priority = (Priority)Enum.Parse(typeof(Priority), collection["Priority"].ToString());
+                    newTask.DeadLine = DateTime.Parse(collection["Deadline"]);
+                    newTask.ProjectId = int.Parse(collection["ProjectId"].ToString());
+
+                    //default value for a new created task
+                    newTask.PercentageCompleted = 0;
+                    newTask.StartDate = DateTime.Today;
+                    newTask.IsCompleted = false;
+                    _db.Tasks.Add(newTask);
+                    _db.SaveChanges();
+
+                    return RedirectToAction("Details", "ProjectHelper", new
+                    {
+                        projectId = newTask.ProjectId
+                    });
+                }
+                else
+                {
+                    return BadRequest();
+                }
             }
             catch
             {
