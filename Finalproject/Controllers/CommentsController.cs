a1651c78696be4d4e34ec3c4af9a2bf11e06b5b4
Nicholas Hacault <nicholashacault@student.mitt.ca>
2022-04-10 02:56:07 -0500

    Add Initial Comment Controller

---
+++ Finalproject/Controllers/CommentsController.cs
@@ -0,0 +1,28 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Finalproject.Data;
+using Finalproject.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace Finalproject.Controllers
+{
+    public class CommentsController : Controller
+    {
+        private ApplicationDbContext _db;
+        private UserManager<ApplicationUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+
+        public CommentsController(ApplicationDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _db = db;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+    }
+}
